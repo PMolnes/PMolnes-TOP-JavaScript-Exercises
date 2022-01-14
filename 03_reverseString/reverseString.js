780817b86e81271ecb6c11e0ccd0e0d2f3291c18
PMolnes <pettermolnes@icloud.com>
2022-01-14 12:59:57 +0100

    Finished exercise 3.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
 const reverseString = function(str) {
-
+    const splitStr = str.split("");
+    const reversedArray = splitStr.reverse();
+    return reversedArray.join(""); 
 };
 
 // Do not edit below this line
