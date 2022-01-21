ba8b362eeed948f34181ea10db52c28d12b4d9cf
PMolnes <pettermolnes@icloud.com>
2022-01-21 14:24:36 +0100

    Finished exercise 06.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,18 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    let isLeapYear = false;
+    // leap years are divisible by four
+    // if divisble by 100, must be divisible by 400
+    if (year % 4 === 0) {
+        if (year % 100 === 0 && year % 400 === 0) {
+            isLeapYear = true;
+        } else if (year % 100 !== 0) {
+            isLeapYear = true;
+        }
+    }
+    return isLeapYear;
 };
+
+leapYears(2200);
 
 // Do not edit below this line
 module.exports = leapYears;
