b3fbd38b0fd6b0e7cb32358797f0718a8e68fe69
PMolnes <pettermolnes@icloud.com>
2022-01-21 13:45:04 +0100

    Finished exercise 05.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
-
+const sumAll = function(start, limit) {
+    if (typeof(start) !== "number" || typeof(limit) !== "number") {
+        return "ERROR";
+    }
+    if (start < 0 || limit < 0) {
+        return "ERROR";
+    } else {
+        let sumAll = 0;
+        let a = start;
+        let b = limit;
+        if (a > b) {
+            let temp;
+            temp = a;
+            a = b;
+            b = temp;
+        }
+        for (i = a; i <= b; i++) {
+            sumAll += i;
+        }
+        return sumAll;
+    }
 };
 
 // Do not edit below this line
