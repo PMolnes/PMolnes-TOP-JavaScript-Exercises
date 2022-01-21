15371c89aa99381b4cbad5735307bc1278e0d038
PMolnes <pettermolnes@icloud.com>
2022-01-21 11:28:57 +0100

    Finished exercise 04.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,22 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, numberToRemove) {
+    
+    // go through original array one by one.
+    // check if numbersToRemove includes current value.
+    // if it doesn't, add current value in original array
+    // to the new array
+    let numbersToRemove = Array.from(arguments).slice(1);
+    let newArray = [];
 
-};
+    for (i = 0; i < array.length; i++) {
+        if (!numbersToRemove.includes(array[i])) {
+            newArray.push(array[i]);
+        }
+    }
+    return newArray;
+}
+
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
